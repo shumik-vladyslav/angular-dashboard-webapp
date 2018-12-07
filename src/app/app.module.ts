4b68870a80132153ef7d2dd764867622a8af3c80
shumik.vladyslav <shumik.vladyslav@gmail.com>
2018-12-08 00:05:00 +0200

    added main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule, MatTabsModule, MatCardModule,MatMenuModule, MatToolbarModule, 
+  MatSidenavModule, MatSortModule, MatDividerModule, MatListModule, MatSelectModule, } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTabsModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatListModule,
+    MatSelectModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
