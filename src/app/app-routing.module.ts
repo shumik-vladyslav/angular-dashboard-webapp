f4e50ce5a77fe928d722df7c093b1184c13921d8
Vladyslav <shumik.vladyslav@gmail.com>
2018-12-08 16:39:34 +0200

    add login && dashbord

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashbordComponent } from './pages/dashbord/dashbord.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: DashbordComponent},
+  { path: 'login', pathMatch: 'full', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatInputModule, MatTabsModule, MatCardModule,MatMenuModule, MatToolbarModule, 
   MatSidenavModule, MatSortModule, MatDividerModule, MatListModule, MatSelectModule, } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { DashbordComponent } from './pages/dashbord/dashbord.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbordComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
