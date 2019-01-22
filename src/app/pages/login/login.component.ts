8b47958e2ffe6d64170aac757bfd40cd7de1ae51
shumik.vladyslav <shumik.vladyslav@gmail.com>
2019-01-22 04:16:32 +0200

    fix

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    const httpPostOptions =
+{   
+    headers:
+        new HttpHeaders (
+        {   
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+        }),
+    withCredentials: true,
+};
+
+    this.http.post('http://dev.lineapps.org/dashboard/login', {
+      "username":"vlad_s@mail.ru",
+      "password":"123456789"
+    }, httpPostOptions).subscribe((data) => {
+      console.log(data)
+    })
+   }
 
   ngOnInit() {
   }
